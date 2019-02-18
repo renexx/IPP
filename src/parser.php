9e40c0e131e5d7d74a09b8f2cc4c0dddebceb212
renexx <rbolf.rene@gmail.com>
2019-02-18 23:31:46 +0100

    add to parser

--- src/parser.php
+++ src/parser.php
@@ -1,1 +1,33 @@
-ss
+<?php 
+/**
+ * Project: IPP project
+ *
+ * @brief parser.php
+ * @author René Bolf         <xbolfr00@stud.fit.vutbr.cz>
+ */
+
+class Token
+{
+    public $tokenType;
+    public $tokenAtr;
+    
+    function _construct($tokenType,$tokenAtr)
+    {
+        $this->$tokenType = $tokenType;
+        $this->$tokenAtr = $tokenAtr;
+        
+    }
+}    
+public static function showHelp()
+{
+    echo("este neviem co sem napisem to az potom nakoniec spravim");
+    
+}    
+
+
+
+
+
+
+
+ ?>
