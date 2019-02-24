f070917b1626dd1ac15f8daad1366d03b6f4afbb
renexx <rbolf.rene@gmail.com>
2019-02-24 23:51:38 +0100

    parser regex

--- src/parser.php
+++ src/parser.php
@@ -25,6 +25,20 @@ public static function showHelp()
 }    */
 $objekt = new Parser;
 $objekt->parse();
+$token = new Token;
+$token->_construct();
+class Token
+{
+    public $tokenType;
+    public $tokenAtr;
+
+    public function _construct($tokenType, $tokenAtr)
+    {
+        $this->token_type = $tokenType;
+        $this->token_atr = $tokenAtr;
+    }
+
+}
 class Parser
 {
     public function parse()
@@ -36,63 +50,102 @@ class Parser
         $program = $xml->createElement("program");
         $program->setAttribute("language","IPPcode19");
         $xml->appendChild($program);
-        $instruction = $xml->createElement("instruction");
-        $program->appendChild($instruction);
 
+        $order_counter = 1; //toto je pocitanie order začina od 1
 
-//*********************************************************************************************************
-       $order_counter = 1; //toto je pocitanie order začina od 1
-       $instruction->setAttribute("order",$order_counter); //nastavenie order
-       $order_counter++; //zvysovanie toho countra
-       $instruction->setAttribute("opcode",strtoupper($opcodeName));
 //************* Vytvorenie pola
-       $opcodeName = array("MOVE", //    <var><symb>
-       "CREATEFRAME",
-       "PUSHFRAME",
-       "POPFRAME",
-       "DEFVAR",//<var>
-       "CALL", //<label>
-       "RETURN",
-       "PUSHS",//<symb>
-       "POPS",//<var>
-       "ADD",//<var><symb1><symb2>
-       "SUB",//<var><symb1><symb2>
-       "MUL",//<var><symb1><symb2>
-       "IDIV",//<var><symb1><symb2>
-       "LT",//<var><symb1><symb2>
-       "GT",//<var><symb1><symb2>
-       "EQ",//<var><symb1><symb2>
-       "AND",//<var><symb1><symb2>
-       "OR",//<var><symb1><symb2>
-       "NOT",//<var><symb1><symb2>
-       "INT2CHAR", //<var><symb>
-       "STRI2INT",//<var><symb1><symb2>
-       "READ",//<var><type>
-       "WRITE",//<symb>
-       "CONCAT",//<var><symb1><symb2>
-       "STRLEN",//var><symb>
-       "GETCHAR",//<var><symb1><symb2>
-       "SETCHAR",//<var><symb1><symb2>
-       "TYPE",//var><symb>
-       "LABEL",//<label>
-       "JUMP",//<label>
-       "JUMPIFEQ", //<var><symb1><symb2>
-       "JUMPIFNEQ",//<var><symb1><symb2>
-        "EXIT", //<symb>
-        "DPRINT", //<symb>
-        "BREAK")
-       echo $xml->saveXML();
+       $opcodeName = array("MOVE"=>"MOVE", //    <var><symb>
+       "CREATEFRAME"=>"CREATEFRAME",
+       "PUSHFRAME"=>"PUSHFRAME",
+       "POPFRAME"=>"POPFRAME",
+       "DEFVAR"=>"DEFVAR",//<var>
+       "CALL"=>"CALL", //<label>
+       "RETURN"=>"RETURN",
+       "PUSHS"=>"PUSHS",//<symb>
+       "POPS"=>"POPS",//<var>
+       "ADD"=>"ADD",//<var><symb1><symb2>
+       "SUB"=>"SUB",//<var><symb1><symb2>
+       "MUL"=>"MUL",//<var><symb1><symb2>
+       "IDIV"=>"IDIV",//<var><symb1><symb2>
+       "LT"=>"LT",//<var><symb1><symb2>
+       "GT"=>"GT",//<var><symb1><symb2>
+       "EQ"=>"EQ",//<var><symb1><symb2>
+       "AND"=>"AND",//<var><symb1><symb2>
+       "OR"=>"OR",//<var><symb1><symb2>
+       "NOT"=>"NOT",//<var><symb1><symb2>
+       "INT2CHAR"=>"INT2CHAR", //<var><symb>
+       "STRI2INT"=>"STRI2INT",//<var><symb1><symb2>
+       "READ"=>"READ",//<var><type>
+       "WRITE"=>"WRITE",//<symb>
+       "CONCAT"=>"CONCAT",//<var><symb1><symb2>
+       "STRLEN"=>"STRLEN",//var><symb>
+       "GETCHAR"=>"GETCHAR",//<var><symb1><symb2>
+       "SETCHAR"=>"SETCHAR",//<var><symb1><symb2>
+       "TYPE"=>"TYPE",//var><symb>
+       "LABEL"=>"LABEL",//<label>
+       "JUMP"=>"JUMP",//<label>
+       "JUMPIFEQ"=>"JUMPIFEQ", //<var><symb1><symb2>
+       "JUMPIFNEQ"=>"JUMPIFNEQ",//<var><symb1><symb2>
+        "EXIT"=>"EXIT", //<symb>
+        "DPRINT"=>"DPRINT", //<symb>
+        "BREAK"=>"BREAK");
         while($line = fgets(STDIN)); // nacitanie vstupu ale iba jedneho riadku
         {
             //if ($line == null || $line == "\n"){
-                ;
+            $instruction = $xml->createElement("instruction");
+            $program->appendChild($instruction);
+
+
+            //*********************************************************************************************************
+            $instruction->setAttribute("order",$order_counter); //nastavenie order
+            $order_counter++; //zvysovanie toho countra
+            $instruction->setAttribute("opcode",strtoupper($opcodeName));       /* TODO*/
+            for($i = 1; $i < func_num_args(); $i++)
+            {
+                $argToken = func_get_arg($i);
+                switch($argToken->$tokenType)
+                {
+                    case "var":
+                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
+                        $argElement->setAttribute("type","var");
+                        break;
+                    case 'label':
+                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
+                        $argElement->setAttribute("type","label");
+                        break;
+                    case 'type':
+                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
+                        $argElement->setAttribute("type","type");
+                        break;
+                    case 'int':
+                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
+                        $argElement->setAttribute("type","int");
+                        break;
+                    case 'bool':
+                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
+                        $argElement->setAttribute("type","bool");
+                        break;
+                    case 'string':
+                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
+                        $argElement->setAttribute("type","string");
+                        break;
+                    default:
+                            echo "error";
+                }
+            $instruction->appendChild($argElement);
+            }
             ///}
         }
+
+        echo $xml->saveXML();
     }
 }
-
-
-
+(preg_match('/^(GF|LF|TF)@([a-zA-Z]|[\_\-\$\&\%\*])([a-zA-Z]|[0-9]|[\_\-\$\&\%\*])*$/'//var
+preg_match('/^int@[+-][0-9]+$/'//int
+preg_match('/^bool@(true|false)$/'//bool
+preg_match('/^string@([a-zA-Z]|[0-9]|\\\\[0-9]{3}|[\_\-\$\&\%\*])*$/'//string
+preg_match('/^(int|string|bool)$/', //type
+preg_match('/^([a-zA-Z]|[\_\-\$\&\%\*])([a-zA-Z]|[0-9]|[\_\-\$\&\%\*])*$/' //label
 
 
 
