07b8ba1619f97d61b35b26fa9845b8ed96d4ea72
renexx <rbolf.rene@gmail.com>
2019-02-27 15:49:12 +0100

    neviem co napisat

--- src/parser.php
+++ src/parser.php
@@ -6,18 +6,7 @@
  * @author René Bolf         <xbolfr00@stud.fit.vutbr.cz>
  */
 
-/*class Token
-{
-    public $tokenType;
-    public $tokenAtr;
 
-    function _construct($tokenType,$tokenAtr)
-    {
-        $this->$tokenType = $tokenType;
-        $this->$tokenAtr = $tokenAtr;
-
-    }
-}
 public static function showHelp()
 {
     echo("este neviem co sem napisem to az potom nakoniec spravim");
@@ -27,6 +16,7 @@ $objekt = new Parser;
 $objekt->parse();
 $token = new Token;
 $token->_construct();
+
 class Token
 {
     public $tokenType;
@@ -51,103 +41,138 @@ class Parser
         $program->setAttribute("language","IPPcode19");
         $xml->appendChild($program);
 
-        $order_counter = 1; //toto je pocitanie order začina od 1
+        $order = 1; //toto je pocitanie order začina od 1
 
 //************* Vytvorenie pola
-       $opcodeName = array("MOVE"=>"MOVE", //    <var><symb>
-       "CREATEFRAME"=>"CREATEFRAME",
-       "PUSHFRAME"=>"PUSHFRAME",
-       "POPFRAME"=>"POPFRAME",
-       "DEFVAR"=>"DEFVAR",//<var>
-       "CALL"=>"CALL", //<label>
-       "RETURN"=>"RETURN",
-       "PUSHS"=>"PUSHS",//<symb>
-       "POPS"=>"POPS",//<var>
-       "ADD"=>"ADD",//<var><symb1><symb2>
-       "SUB"=>"SUB",//<var><symb1><symb2>
-       "MUL"=>"MUL",//<var><symb1><symb2>
-       "IDIV"=>"IDIV",//<var><symb1><symb2>
-       "LT"=>"LT",//<var><symb1><symb2>
-       "GT"=>"GT",//<var><symb1><symb2>
-       "EQ"=>"EQ",//<var><symb1><symb2>
-       "AND"=>"AND",//<var><symb1><symb2>
-       "OR"=>"OR",//<var><symb1><symb2>
-       "NOT"=>"NOT",//<var><symb1><symb2>
-       "INT2CHAR"=>"INT2CHAR", //<var><symb>
-       "STRI2INT"=>"STRI2INT",//<var><symb1><symb2>
-       "READ"=>"READ",//<var><type>
-       "WRITE"=>"WRITE",//<symb>
-       "CONCAT"=>"CONCAT",//<var><symb1><symb2>
-       "STRLEN"=>"STRLEN",//var><symb>
-       "GETCHAR"=>"GETCHAR",//<var><symb1><symb2>
-       "SETCHAR"=>"SETCHAR",//<var><symb1><symb2>
-       "TYPE"=>"TYPE",//var><symb>
-       "LABEL"=>"LABEL",//<label>
-       "JUMP"=>"JUMP",//<label>
-       "JUMPIFEQ"=>"JUMPIFEQ", //<var><symb1><symb2>
-       "JUMPIFNEQ"=>"JUMPIFNEQ",//<var><symb1><symb2>
-        "EXIT"=>"EXIT", //<symb>
-        "DPRINT"=>"DPRINT", //<symb>
-        "BREAK"=>"BREAK");
-        while($line = fgets(STDIN)); // nacitanie vstupu ale iba jedneho riadku
+
+        while($line = fgets(STDIN)); // nacitanie vstupu
         {
+
+            $line = preg_replace("~#.*~", "", $line); //zrusenie komentov, medzier
+            $splitLineToWord = preg_split('/\s+/', $line); // rozdelenie stringu na slova
             //if ($line == null || $line == "\n"){
             $instruction = $xml->createElement("instruction");
             $program->appendChild($instruction);
-
-
             //*********************************************************************************************************
-            $instruction->setAttribute("order",$order_counter); //nastavenie order
-            $order_counter++; //zvysovanie toho countra
-            $instruction->setAttribute("opcode",strtoupper($opcodeName));       /* TODO*/
-            for($i = 1; $i < func_num_args(); $i++)
-            {
-                $argToken = func_get_arg($i);
-                switch($argToken->$tokenType)
-                {
-                    case "var":
-                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
-                        $argElement->setAttribute("type","var");
-                        break;
-                    case 'label':
-                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
-                        $argElement->setAttribute("type","label");
-                        break;
-                    case 'type':
-                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
-                        $argElement->setAttribute("type","type");
-                        break;
-                    case 'int':
-                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
-                        $argElement->setAttribute("type","int");
-                        break;
-                    case 'bool':
-                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
-                        $argElement->setAttribute("type","bool");
-                        break;
-                    case 'string':
-                        $argElement = $xml->createElement("arg".$i,$argToken->$tokenAtr);
-                        $argElement->setAttribute("type","string");
-                        break;
-                    default:
-                            echo "error";
-                }
-            $instruction->appendChild($argElement);
+            $instruction->setAttribute("order",$order); //nastavenie order
+            $order++; //zvysovanie toho countra
+
+            if(!preg_match('/^(.IPPcode19)*$/i'))
             }
-            ///}
+                fprint(STDERR, "Missing header");// hlavicka //  /i znamena ze je to case insensitive
+                return 21; // navratovy kod pre chybnu hlavicku
+            {
+
+
+                $opcodeName[0] = strtoupper($splitLineToWord[0]);
+                $instruction->setAttribute("opcode",strtoupper($opcodeName));
+                switch($opcodeName[0] = strtoupper($splitLineToWord[0]))
+                {
+/******************  0 operand *****************************/
+                    case "CREATEFRAME":
+                    case "PUSHFRAME":
+                    case "POPFRAME":
+                    case "RETURN":
+                    case "BREAK":
+                        if (count($splitLineToWord) != 1)
+                            return 23;
+
+                        break;
+/****************** 1 operand <var>********************************************/
+                    case "DEFVAR":
+                    case "POPS":
+                        if (count($splitLineToWord) != 2)
+                            return 23;
+                        check_var();
+                        break;
+/****************** 1 operand <symb>*******************************************/
+                    case "PUSH":
+                    case "WRITE":
+                    case "EXIT":
+                    case "DPRINT":
+                        if (count($splitLineToWord) != 2)
+                            return 23;
+                        check_symb;
+                        break;
+/****************** 1 operand <label>******************************************/
+                    case "CALL":
+                    case "LABEL":
+                    case "JUMP":
+                        if (count($splitLineToWord) != 2)
+                            return 23;
+                        check_label();
+                        break;
+/****************** 2 operands <var><symb>*************************************/
+                    case "MOVE":
+                    case "INT2CHAR":
+                    case "TYPE":
+                    case "STRLEN":
+                        if (count($splitLineToWord) != 3)
+                            return 23;
+                        check_var();
+                        check_symb();
+                        break;
+/****************** 2 operands <var><type>*************************************/
+                    case "READ":
+                        if (count($splitLineToWord) != 3)
+                            return 23;
+                        check_var();
+                        check_type();
+                        break;
+/****************** 3 operands <var><symb1><symb2>*****************************/
+                    case "ADD":
+                    case "SUB":
+                    case "MUL":
+                    case "IDIV":
+                    case "LT":
+                    case "GT":
+                    case "EQ":
+                    case "AND":
+                    case "OR":
+                    case "NOT":
+                    case "STRI2INT":
+                    case "CONCAT":
+                    case "GETCHAR":
+                    case "SETCHAR":
+                        if (count($splitLineToWord) != 4)
+                            return 23;
+                            check_var();
+                            check_symb();
+                            check_symb();
+                        break;
+                    //-- 3 operand <labe><symb1><symb2>
+                    case "JUMPIFEQ":
+                    case "JUMPIFNEQ":
+                        if (count($splitLineToWord) != 4)
+                            return 23;
+                            check_label();
+                            check_symb();
+                            check_symb();
+                        break;
+                }
+
         }
 
         echo $xml->saveXML();
     }
+    public function check_label()
+    {
+        if(preg_match('/^(GF|LF|TF)@([a-zA-Z]|[\_\-\$\&\%\*])([a-zA-Z]|[0-9]|[\_\-\$\&\%\*])*$/')
+    }
+    public function check_var()
+    {
+        if(preg_match('/^(GF|LF|TF)@([a-zA-Z]|[\_\-\$\&\%\*])([a-zA-Z]|[0-9]|[\_\-\$\&\%\*])*$/')
+    }
+    public function check_type()
+    {
+        if(preg_match('/^(int|string|bool)$/')
+    }
+    public function check_symb()
+    {
+        if(preg_match('/^int@[+-][0-9]+$/')
+        if(preg_match('/^bool@(true|false)$/')
+        if(preg_match('/^string@([a-zA-Z]|[0-9]|\\\\[0-9]{3}|[\_\-\$\&\%\*])*$/'/)
+    }
 }
-(preg_match('/^(GF|LF|TF)@([a-zA-Z]|[\_\-\$\&\%\*])([a-zA-Z]|[0-9]|[\_\-\$\&\%\*])*$/'//var
-preg_match('/^int@[+-][0-9]+$/'//int
-preg_match('/^bool@(true|false)$/'//bool
-preg_match('/^string@([a-zA-Z]|[0-9]|\\\\[0-9]{3}|[\_\-\$\&\%\*])*$/'//string
-preg_match('/^(int|string|bool)$/', //type
-preg_match('/^([a-zA-Z]|[\_\-\$\&\%\*])([a-zA-Z]|[0-9]|[\_\-\$\&\%\*])*$/' //label
-
-
-
 
  ?>
