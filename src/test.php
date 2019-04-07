d175d6edbe5b3ba929116e9ce8a0522237a20606
renexx <rbolf.rene@gmail.com>
2019-04-07 16:51:08 +0200

    vygenerovane ak tam niesu

--- src/test.php
+++ src/test.php
@@ -32,11 +32,15 @@ class CheckArgumentsAndError
         $this->directory = $this->directory."/";
     }
 
-    public function parseArguments($argc,$argv)
+    public function parseArguments($argv,$argc)
     {
 
         $options = array("help","directory::","recursive","parse-script::","int-script::","parse-only","int-only");
         $opts = getopt("",$options);
+        if(count($opts) != $argc - 1)
+        {
+            self::errorMessage("Bad arguments",10);
+        }
         if($argc === 2)
         {
             if(isset($opts["help"]))
@@ -46,7 +50,7 @@ class CheckArgumentsAndError
                     self::showHelp();
                 }
             }
-            elseif(isset($opts["directory"]))
+            if(isset($opts["directory"]))
             {
                 if(!is_dir($opts["directory"]))
                 {
@@ -57,46 +61,43 @@ class CheckArgumentsAndError
                 var_dump($this->directory);
 
             }
-            elseif(isset($opts["recursive"]))
+            if(isset($opts["recursive"]))
             {
                 $this->recursive = true;
             }
-            elseif(isset($opts["parse-script"]))
+            if(isset($opts["parse-script"]))
             {
                 $this->parser = $opts["parse-script"];
             }
-            elseif(isset($opts["int-script"]))
+            if(isset($opts["int-script"]))
             {
                 $this->interpret = $opts["int-script"];
             }
-            elseif(isset($opts["parse-only"]))
+            if(isset($opts["parse-only"]))
             {
                 $this->parseonly = true;
             }
-            elseif(isset($opts["int-only"]))
+            if(isset($opts["int-only"]))
             {
                 $this->intonly = true;
             }
-            else
-            {
-                self::errorMessage("Bad arguments",10);
-            }
+
         }
-        if($argc > 2)
+        elseif($argc > 2)
         {
             if(isset($opts["parse-only"]) && isset($opts["int-only"]))
             {
                 self::errorMessage("parse-only and int-only can´t be combinated",10);
             }
-            elseif(isset($opts["parse-only"]) && isset($opts["int-script"]))
+            if(isset($opts["parse-only"]) && isset($opts["int-script"]))
             {
                 self::errorMessage("parse-only and int-script can´t be combinated",10);
             }
-            elseif(isset($opts["int-only"]) && isset($opts["parse-only"]))
+            if(isset($opts["int-only"]) && isset($opts["parse-only"]))
             {
                 self::errorMessage("int-only and parse-only can´t be combinated",10);
             }
-            elseif(isset($opts["int-only"]) && isset($opts["parse-script"]))
+            if(isset($opts["int-only"]) && isset($opts["parse-script"]))
             {
                 self::errorMessage("int-only and parse-script can´t be combinated",10);
             }
@@ -107,32 +108,27 @@ class CheckArgumentsAndError
                     self::errorMessage("Specified directory is not a directory",11);
                 }
                 $this->directory = realpath($opts["directory"])."/";
-                print("riadok 110");
-                var_dump($this->directory);
+
             }
-            elseif(isset($opts["recursive"]))
+            if(isset($opts["recursive"]))
             {
                 $this->recursive = true;
             }
-            elseif(isset($opts["parse-script"]))
+            if(isset($opts["parse-script"]))
             {
                 $this->parser = $opts["parse-script"];
             }
-            elseif(isset($opts["int-script"]))
+            if(isset($opts["int-script"]))
             {
                 $this->interpret = $opts["int-script"];
             }
-            elseif(isset($opts["parse-only"]))
+            if(isset($opts["parse-only"]))
             {
                 $this->parseonly = true;
             }
-            elseif(isset($opts["int-only"]))
+            if(isset($opts["int-only"]))
             {
                 $this->intonly = true;
-            }
-            else
-            {
-                self::errorMessage("Bad arguments",10);
             }
         }
     }
@@ -171,31 +167,81 @@ class CheckArgumentsAndError
 
 class Test extends CheckArgumentsAndError
 {
+    public $tests = array();
 
     public function runTest()
     {
+        $in = false;
+        $out = false;
+        $rc = false;
+        $tests;
         CheckArgumentsAndError::parseArguments($GLOBALS["argv"],$GLOBALS["argc"]);
-        $files = scandir($this->directory);
-        var_dump($this->directory);
-        var_dump($files);
-        $base = basename($this->directory);
-        var_dump($this->directory);
+        self::recursive($this->directory);
+        //var_dump($this->tests);
+        foreach($this->tests as $files)
+        {
+            $daco = pathinfo($files);
+            if($files == $daco["filename"].".in")
+            {
+                $in = true;
+            }
+            if($files == $daco["filename"].".out")
+            {
+                $out = true;
+            }
+            if($files == $daco["filename"].".rc")
+            {
+                $rc = true;
+            }
+//////////// VYTVARANIE ////////////////
+            if($in == false)
+            {
+                exec("touch ". $daco["dirname"] . "/". $daco["filename"].".in",$output,$return_var);
+                if($return_var == 1)
+                {
+                    CheckArgumentsAndError::errorMessage("Couldn´t generate .in file",12);
+                }
+            }
+            if($out == false)
+            {
+                exec("touch ". $daco["dirname"] . "/". $daco["filename"].".out",$output,$return_var);
+                if($return_var == 1)
+                {
+                    CheckArgumentsAndError::errorMessage("Couldn´t generate .out file",12);
+                }
+            }
+            if($rc == false)
+            {
+                exec("touch ". $daco["dirname"] . "/". $daco["filename"].".rc",$output,$return_var);
+                exec("echo 0 >>" .  $daco["dirname"]."/".$daco["filename"].".rc");
+                if($return_var == 1)
+                {
+                    CheckArgumentsAndError::errorMessage("Couldn´t generate .rc file",12);
+                }
+            }
+        }
+    }
+
+    public function recursive($dir)
+    {
+        $files = scandir($dir);
         foreach($files as $file)
         {
-            if(is_dir($this->directory.$file))
+            $file = $dir.$file; // cesta skonkatenovana s nazvom file a je to ulozene do file
+            if(is_dir($file)) //ak je priecinok ten file s celou cestou a je nastavena rekurzia tak preskoc . a .. a rekurzivne zavolaj funkciu
             {
                 if($this->recursive == true)
-                    if($file == "." || $file == "..")
-                    continue;
-                    self::runTest($this->directory . $file . "/");
-            }
-                else
                 {
-                    continue;
+                    if($file == $dir."." || $file == $dir."..")
+                        continue;
+                    self::recursive($file ."/");
                 }
+            }
+            else
+            {
+                array_push($this->tests,$file); // do pola ulozime vsetky subory ktore niesu dir
+            }
         }
-        var_dump($files);
-
     }
 }
 
